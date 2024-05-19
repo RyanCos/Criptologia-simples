4915d87bb9ad002a77440772849b7fa25488de2c
Ryan <Ryancostaf@gmail.com>
2024-05-19 09:29:18 -0300

    First commit

---
+++ CifrasPoli.py
@@ -0,0 +1,7 @@
+P_chave = input("Digite qual será a sua senha:")
+P_cript = input("Digite qual será a palavra criptgrafica:")
+alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+
+print(alfabeto)
